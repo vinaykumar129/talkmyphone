ef870b70338dc6457820ba9779296e705acbfdca
florent lopez <florent.lopez@gmail.com>
2010-10-12 00:09:45 +0200

    Add 2 preferences - to show sent sms - number of sms to display
    Reformat Talkmyphone response to contact and sms (with bold & italic on gtalk & pidgin)
    Add possiblity to show sent sms on sms:#contact# command
    Sort sms by contact names and dates (on multiple matching)

--- src/com/googlecode/talkmyphone/sms/Sms.java
+++ src/com/googlecode/talkmyphone/sms/Sms.java
@@ -2,7 +2,14 @@ package com.googlecode.talkmyphone.sms;
 
 import java.util.Date;
 
-public class Sms {
+public class Sms implements Comparable<Sms> {
     public String message;
+    public String number;
+    public String sender;
     public Date date;
+    
+    @Override
+    public int compareTo(Sms another) {
+        return date.compareTo(another.date);
+    }
 }
